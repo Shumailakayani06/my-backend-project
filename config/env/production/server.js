f572f122b57653bd81bb0d0d969396b881a01b9e
Shumaila Kayani <shumailakayani059@gmail.com>
2022-05-30 16:10:36 +0200

    removed extra server

--- config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-    url: env("https://my-holidaze-backend.herokuapp.com/"),
-    proxy: true,
-    app: {
-      keys: env.array("APP_KEYS", ["Key1", "Key2"]),
-    },
-  });
